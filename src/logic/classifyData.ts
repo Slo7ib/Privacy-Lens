3af9e02e30de496896361a89bf901356ade0343c
Saleh Blhmr <83185094+Slo7ib@users.noreply.github.com>
2025-11-29 16:52:47 +0300

    Add collectedCategories data structure
    Introduces the CollectedCategory interface and a collectedCategories array to define categories for rendering elements in React based on API responses. Each category includes section, element, collected status, and icon.

---
+++ src/logic/classifyData.ts
@@ -0,0 +1,77 @@
+// Here i will be defining a category that should be connected to the react component to render the elemnts based on the response from the api
+import type { LucideIcon } from "lucide-react";
+import {
+  Phone,
+  KeyRound,
+  UserRound,
+  Mail,
+  History,
+  Link,
+  Cookie,
+  HouseWifi,
+  MonitorSmartphone,
+} from "lucide-react";
+
+export interface CollectedCategory {
+  section: string;
+  element: string;
+  collected: boolean;
+  icon: LucideIcon;
+}
+
+const collectedCategories: CollectedCategory[] = [
+  {
+    element: "Phone Number",
+    collected: false,
+    icon: Phone,
+    section: "Personal Identifiers",
+  },
+  {
+    element: "Account Credentials",
+    collected: false,
+    icon: KeyRound,
+    section: "Personal Identifiers",
+  },
+  {
+    element: "Personal Information",
+    collected: false,
+    icon: UserRound,
+    section: "Personal Identifiers",
+  },
+  {
+    element: "Email",
+    collected: false,
+    icon: Mail,
+    section: "Personal Identifiers",
+  },
+  {
+    element: "Links clicked",
+    collected: false,
+    icon: Link,
+    section: "Online Activity",
+  },
+  {
+    element: "Pages visited",
+    collected: false,
+    icon: History,
+    section: "Online Activity",
+  },
+  {
+    element: "Cookie usage",
+    collected: false,
+    icon: Cookie,
+    section: "Online Activity",
+  },
+  {
+    element: "Deivce Type",
+    collected: false,
+    icon: MonitorSmartphone,
+    section: "Device Data",
+  },
+  {
+    element: "IP Address",
+    collected: false,
+    icon: HouseWifi,
+    section: "Device Data",
+  },
+];
